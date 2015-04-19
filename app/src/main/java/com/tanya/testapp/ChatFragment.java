3b43544d324391a3441079f3732987276178b1fd
tparnyuk <t.parnyukt@videal.net>
2015-04-19 23:03:25 +0300

    Create Fragments. Add images.

---
+++ app/src/main/java/com/tanya/testapp/ChatFragment.java
@@ -0,0 +1,22 @@
+package com.tanya.testapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChatFragment extends android.support.v4.app.Fragment {
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+
+}
--- app/src/main/java/com/tanya/testapp/MainActivity.java
+++ app/src/main/java/com/tanya/testapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.tanya.testapp;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,28 +12,32 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        ActionBar.Tab tab = actionBar.newTab()
+                .setIcon(R.drawable.chat)
+                .setTabListener(new TabListener<>(
+                        this, "chat", ChatFragment.class));
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab()
+                .setIcon(R.drawable.user_male_blue)
+                .setTabListener(new TabListener<>(
+                        this, "contacts", UsersFragment.class));
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab()
+                .setIcon(R.drawable.tag_blue)
+                .setTabListener(new TabListener<>(
+                        this, "notes", NotesFragment.class));
+        actionBar.addTab(tab);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
